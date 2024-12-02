828c9eb191dda46502ee876ef0be0a75cc818f95
Kevin Tatooles <kevintatooles@gmail.com>
2024-12-01 21:07:31 -0600

    Day 1 part 1

--- Day 1/a.ts
+++ Day 1/a.ts
@@ -1,1 +1,27 @@
-console.log("test deno project"); 
+const input = await Deno.readTextFile("input.txt");
+
+// Convert text into two lists, one for each side
+const arr = input.split(/\r?\n/);
+
+
+const a = [];
+const b = [];
+
+for(const pair of arr) {
+  const split = pair.split("  ");
+  a.push(split[0]);
+  b.push(split[1]);
+}
+
+a.sort();
+b.sort();
+
+// Now that it's sorted, we go down the line and compare
+
+let difference = 0;
+
+for(let i = 0; i < a.length; i++) {
+  difference += Math.abs(parseInt(a[i]) - parseInt(b[i]));
+}
+
+console.log(difference);
