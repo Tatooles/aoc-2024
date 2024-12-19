8e56c536cc38939efb40d715ee6e4f8c84744272
Kevin Tatooles <kevintatooles@gmail.com>
2024-12-19 16:23:33 -0600

    Day 8 p1 solution, aside from when antinodes overlap

--- Day 8/a.ts
+++ Day 8/a.ts
@@ -9,21 +9,25 @@ const printGrid = (gridInstance: string[][]) => {
   console.log();
 };
 
-printGrid(grid);
-
-const frequencies = new Set<string>();
+const frequencies = new Map<string, number[][]>();
 
 // Find each individual frequenct
-grid.forEach((row) => {
-  row.forEach((val) => {
-    if (val !== ".") frequencies.add(val);
+grid.forEach((row, i) => {
+  row.forEach((val, j) => {
+    if (val !== ".") {
+      if (!frequencies.has(val)) {
+        frequencies.set(val, []);
+      }
+      frequencies.get(val)!.push([i, j]);
+    }
   });
 });
 
-const countAntinodes = (frequency: string) => {
+const countAntinodes = (frequency: string): number => {
   // Create a copy of the grid to play with
+  let frequencyCount = 0;
+
   const gridCopy: string[][] = JSON.parse(JSON.stringify(grid));
-  printGrid(gridCopy);
 
   // Clear grid to make things easier
   for (let i = 0; i < gridCopy.length; i++) {
@@ -34,23 +38,52 @@ const countAntinodes = (frequency: string) => {
     }
   }
 
+  const locations = frequencies.get(frequency);
+
+  if (!locations?.length || locations?.length < 2) {
+    return 0;
+  }
+
+  // Go through every possible combo of towers
+  for (let first = 0; first < locations.length; first++) {
+    for (let second = first + 1; second < locations.length; second++) {
+      // Compare the two
+      let diffRow = locations[second][0] - locations[first][0];
+      let diffCol = locations[second][1] - locations[first][1];
+      console.log("diff row, col", diffRow, diffCol);
+
+      // Attempt to place a thing on both sides of each, 2 total locations!
+      // Try 1st oppsite of second
+      try {
+        gridCopy[locations[first][0] - diffRow][locations[first][1] - diffCol] =
+          "#";
+        frequencyCount++;
+      } catch (_e) {
+        // doesnt matter
+      }
+
+      try {
+        gridCopy[locations[second][0] + diffRow][
+          locations[second][1] + diffCol
+        ] = "#";
+        frequencyCount++;
+      } catch (_e) {
+        // dont matta
+      }
+      // Maybe just toss it in a try catch instead of checking for errors LOL
+    }
+  }
   printGrid(gridCopy);
 
-  // gridCopy.forEach((row) => {
-  //   row.forEach((val) => {
-  //     if (val !== frequency) {
-  //       // Do something
-  //       // The difference between one tower and another is the difference between that tower and the antinode
-  //       // It's just that a lot of them fall outside the screen!
-  //     }
-  //   });
-  // });
-
-  return 1;
+  return frequencyCount;
 };
+
+console.log(frequencies);
 
 let count = 0;
 // Need to count number of antinodes per frequency
-for (const frequency of frequencies) {
+for (const frequency of frequencies.keys()) {
   count += countAntinodes(frequency);
 }
+
+console.log(count);
