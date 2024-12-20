864ac08873c80e639a5d95ccd53ba51b3ba62e94
Kevin Tatooles <kevintatooles@gmail.com>
2024-12-20 14:15:20 -0600

    Day 8 part 1 cleanup

--- Day 8/a.ts
+++ Day 8/a.ts
@@ -1,7 +1,29 @@
-const input = await Deno.readTextFile("input.txt");
+let grid: string[][];
 
-const grid = input.split(/\r?\n/).map((x) => x.split(""));
+// Store frequencies in map, Frequency is key, List of locations is value
+const antennas = new Map<string, number[][]>();
 
+// The locations of the antinodes are stored in set to prevent duplicates
+const antinodes = new Set<string>();
+
+const main = async () => {
+  const input = await Deno.readTextFile("input.txt");
+
+  grid = input.split(/\r?\n/).map((x) => x.split(""));
+
+  createMap();
+
+  for (const frequency of antennas.keys()) {
+    countAntinodes(frequency);
+  }
+
+  console.log(antinodes.size);
+};
+
+/**
+ * Helper function to print grid
+ * @param gridInstance
+ */
 const printGrid = (gridInstance: string[][]) => {
   for (const row of gridInstance) {
     console.log(row.join(""));
@@ -9,75 +31,58 @@ const printGrid = (gridInstance: string[][]) => {
   console.log();
 };
 
-const frequencies = new Map<string, number[][]>();
-
-// The locations of the antinodes are stored here
-const antinodes = new Set<string>();
-
-// Find each individual frequenct
-grid.forEach((row, i) => {
-  row.forEach((val, j) => {
-    if (val !== ".") {
-      if (!frequencies.has(val)) {
-        frequencies.set(val, []);
+/**
+ * Find each individual frequency and build map
+ * with it's locations in the grid
+ */
+const createMap = () => {
+  grid.forEach((row, i) => {
+    row.forEach((val, j) => {
+      if (val !== ".") {
+        if (!antennas.has(val)) {
+          antennas.set(val, []);
+        }
+        antennas.get(val)!.push([i, j]);
       }
-      frequencies.get(val)!.push([i, j]);
-    }
+    });
   });
-});
+};
 
+/**
+ * Goes through every possible combination of towers for this frequency,
+ * compares the two, and determines if an antinode can go oppsite each one.
+ *
+ * @param frequency - Which towers to look at
+ * @returns
+ */
 const countAntinodes = (frequency: string) => {
-  // Create a copy of the grid to play with
-  const gridCopy: string[][] = JSON.parse(JSON.stringify(grid));
-
-  // Clear grid to make things easier
-  for (let i = 0; i < gridCopy.length; i++) {
-    for (let j = 0; j < gridCopy[i].length; j++) {
-      if (gridCopy[i][j] !== frequency) {
-        gridCopy[i][j] = ".";
-      }
-    }
-  }
-
-  const locations = frequencies.get(frequency);
+  const locations = antennas.get(frequency);
 
   if (!locations?.length || locations?.length < 2) {
     return;
   }
 
-  // Go through every possible combo of towers
   for (let first = 0; first < locations.length; first++) {
     for (let second = first + 1; second < locations.length; second++) {
-      // Compare the two
       const diffRow = locations[second][0] - locations[first][0];
       const diffCol = locations[second][1] - locations[first][1];
-      // console.log("diff row, col", diffRow, diffCol);
 
-      // Attempt to place a thing on both sides of each, 2 total locations!
-      // Try 1st oppsite of second
       let i = locations[first][0] - diffRow;
       let j = locations[first][1] - diffCol;
-      if (i > -1 && i < gridCopy.length && j > -1 && j < gridCopy[i].length) {
+      if (i > -1 && i < grid.length && j > -1 && j < grid[i].length) {
         antinodes.add(`${i},${j}`);
       }
 
       i = locations[second][0] + diffRow;
       j = locations[second][1] + diffCol;
 
-      if (i > -1 && i < gridCopy.length && j > -1 && j < gridCopy[i].length) {
+      if (i > -1 && i < grid.length && j > -1 && j < grid[i].length) {
         antinodes.add(`${i},${j}`);
       }
-      // Maybe just toss it in a try catch instead of checking for errors LOL
     }
   }
-  // printGrid(gridCopy);
 };
 
-// Need to count number of antinodes per frequency
-for (const frequency of frequencies.keys()) {
-  countAntinodes(frequency);
-}
-
-console.log(antinodes.size);
-
-// TODO: Getting lots of negaive antinodes, fix that
+console.time("execution time");
+main();
+console.timeEnd("execution time");
